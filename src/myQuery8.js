ed226d5d6ee0c0b8276438ea3264e68982bf18fa
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-21 18:39:16 +0530

    Changes Done with Focus And Keydown Method

--- src/myQuery8.js
+++ src/myQuery8.js
@@ -4,13 +4,29 @@ $(document).ready(function () {
     var b = $("#last").val();
     if (a == "" && b == "") {
       $("#first").css("background-color", "Red");
+      $('#first').focus(); // Focus method used 
+      $("#first").keydown(function(){ // Keydown method is used to change the color of the field
+        $("#first").css("background-color", "white");
+      });
       $("#last").css("background-color", "Red");
+       $("#last").keydown(function(){
+        $("#last").css("background-color", "white");
+       });
+      
       alert("Field Can Not Be Empty");
     } else if (a == "") {
       $("#first").css("background-color", "Red");
+      $("#first").keydown(function(){
+        $("input").css("background-color", "white");
+      });
+      $('#first').focus();
       alert("Field Can Not Be Empty");
     } else if (b == "") {
       $("#last").css("background-color", "Red");
+      $("#last").keydown(function(){
+        $("#last").css("background-color", "white");
+      });
+      $('#last').focus();
       alert("Field Can Not Be Empty");
     }
   });
