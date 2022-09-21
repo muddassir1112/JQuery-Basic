6493694c2a01cc4c7c4f87bcc76c22e97fc25db5
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-21 18:37:57 +0530

    Changes Done with Focus And Keydown Method

--- src/myQuery2.js
+++ src/myQuery2.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-    $("p").first().animate({color:'red'},5000);
-    $("p").last().animate({color:'red'},5000);
+     $("p").first().animate({color:'red'},5000);
+    //  $("p").siblings().animate({color:'red'},5000);
+     $("p").last().animate({color:'red'},5000);
+    // $(" .test :first-child").animate({color:'red'},5000);
+    // $(".test p").last().animate({color:'red'},5000);
 });
