f7732f74ffd442ddcfbcd7dae34c8e8aa5814422
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-22 12:46:42 +0530

    Changes Done with given instructions

--- src/myQuery7.js
+++ src/myQuery7.js
@@ -2,10 +2,25 @@ $(document).ready(function () {
   $("#input2").keyup(function () {
     var pass = $("#input1").val();
     var conPass = $("#input2").val();
+    $("p").text("Password Not Match").css("color", "red").hide();
+
     if (pass == conPass) {
-      $("p").text("Password Match");
-    } else {
-      $("p").text("Password Not Match");
+      $("p").text("Password Match").css("color", "black").show();
+    } else if (conPass == "") {
+      $("p").text("Password Not Match").css("color", "red").hide();
+    } else if (pass != conPass) {
+      $("p").text("Password Not Match").css("color", "red").show();
+    }
+  });
+  $("#input1").keyup(function () {
+    var pass = $("#input1").val();
+    var conPass = $("#input2").val();
+    if (pass == "" || conPass == "") {
+      $("p").text("Password Match").css("color", "black").hide();
+    } else if (conPass != pass) {
+      $("p").text("Password Not Match").css("color", "red").show();
+    } else if (conPass == pass) {
+      $("p").text("Password Match").css("color", "black").show();
     }
   });
 });
