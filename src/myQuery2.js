3ceacdf7e9b74e6c9bce61dfb353068614b53151
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-22 13:10:19 +0530

    First and Last Child of test class has been animated

--- src/myQuery2.js
+++ src/myQuery2.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-     $("p").first().animate({color:'red'},5000);
-    //  $("p").siblings().animate({color:'red'},5000);
-     $("p").last().animate({color:'red'},5000);
-    // $(" .test :first-child").animate({color:'red'},5000);
-    // $(".test p").last().animate({color:'red'},5000);
+     $(".test p:first-child").animate({color:'red'},5000);
+     $(".test p:last-child").animate({color:'red'},5000);
 });
