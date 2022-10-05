bfcac1b114b9f0a008fc7e2b0d8559aa5ae19994
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-10-05 18:51:10 +0530

    Working with newly created list items

--- src/myQuery4.js
+++ src/myQuery4.js
@@ -1,79 +1,76 @@
-"use strict";
-$(document).ready(function () {
-  var category;
-
-  $("#button1").click(function () {
+var category;
+$(document).ready(function(){
+   C_Ul1 = () => {
     category = prompt("Enter The Category");
     if (category != "") {
       $("#subul1").append(
         "<li><a href='#'>" +
           category +
-          "</a><input type='button' value='+'></li>"
+          "</a><input type='button' value='+' class='button1' onclick='C_Ul1()'></li>"
       );
-    } else {
+    }
+    else {
       alert("Add atleast one item");
     }
-  });
-  $("#button2").click(function () {
+  }
+  Tshirt_l1 = () => {
     category = prompt("Enter The Sub Category");
     if (category != "") {
       $("#l1").append(
         "<ul><li><a href='#'>" +
           category +
-          "</a><input type='button' value='+'></li></ul>"
+          "</a><input type='button' value='+' onclick='Tshirt_l1()'></li></ul>"
       );
     } else {
       alert("Add atleast one item");
     }
-  });
-
-  $("#button3").click(function () {
+  }
+   Trouser_l2 = () => {
     category = prompt("Enter The Sub Category");
     if (category != "") {
       $("#l2").append(
         "<ul><li><a href='#'>" +
           category +
-          "</a><input type='button' value='+'></li></ul>"
+          "</a><input type='button' value='+' onclick='Trouser_l2()'></li></ul>"
       );
     } else {
       alert("Add atleast one item");
     }
-  });
-  $("#button4").click(function () {
+  }
+  Elec_Ul2 = () => {
     category = prompt("Enter The Category");
     if (category != "") {
       $("#subul2").append(
         "<li><a href='#'>" +
           category +
-          "</a><input type='button' value='+'></li>"
+          "</a><input type='button' value='+' onclick='Elec_Ul2()'></li>"
       );
     } else {
       alert("Add atleast one item");
     }
-  });
-  $("#button5").click(function () {
+  }
+  Mob_l3 = () => {
     category = prompt("Enter The Sub Category");
     if (category != "") {
       $("#l3").append(
         "<ul><li><a href='#'>" +
           category +
-          "</a><input type='button' value='+'></li></ul>"
+          "</a><input type='button' value='+' onclick='Mob_l3()'></li></ul>"
       );
     } else {
       alert("Add atleast one item");
     }
-  });
-
-  $("#button6").click(function () {
+  }
+   Tabs_l4 = () => {
     category = prompt("Enter The Sub Category");
     if (category != "") {
       $("#l4").append(
         "<ul><li><a href='#'>" +
           category +
-          "</a><input type='button' value='+'></li></ul>"
+          "</a><input type='button' value='+' onclick='Tabs_l4()'></li></ul>"
       );
     } else {
       alert("Add atleast one item");
     }
-  });
+  }
 });
