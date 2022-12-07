ba74965d337c63133e7e26f4083f31bd6c1e7e8c
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-12-07 12:11:32 +0530

    Task 4 modified

--- src/myQuery3.js
+++ src/myQuery3.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $(".table").click(function(event){
+$(document).ready(function () {
+  $(".table").click(function (event) {
     let str =
       "<br> <div><input type='text' placeholder='Enter Class'> <input type='text' placeholder='Enter Board'> <input type='text' placeholder='Enter Marks'>  <input type='text' placeholder='Enter Division'> <button class='add'>+</button>  <button class='rmvRow'>-</button> </div>";
     if (event.target.className == "add") {
--- src/myQuery4.js
+++ src/myQuery4.js
@@ -1,76 +1,49 @@
-var category;
-$(document).ready(function(){
-   C_Ul1 = () => {
-    category = prompt("Enter The Category");
-    if (category != "") {
-      $("#subul1").append(
-        "<li><a href='#'>" +
-          category +
-          "</a><input type='button' value='+' class='button1' onclick='C_Ul1()'></li>"
-      );
-    }
-    else {
-      alert("Add atleast one item");
-    }
+$(() => {});
+function addingClothes() {
+  let clothName = prompt("Cloth Name");
+  if (clothName == "") {
+    alert("Add the data");
+  } else {
+    $("#clothes").append(
+      "<li><a href='#'>" +
+        clothName +
+        "</a><button onclick='addingMoreClothes()'>+</button></li>"
+    );
   }
-  Tshirt_l1 = () => {
-    category = prompt("Enter The Sub Category");
-    if (category != "") {
-      $("#l1").append(
-        "<ul><li><a href='#'>" +
-          category +
-          "</a><input type='button' value='+' onclick='Tshirt_l1()'></li></ul>"
-      );
-    } else {
-      alert("Add atleast one item");
-    }
+}
+function addingMoreClothes(){
+   let clothName = prompt("Cloth Name");
+  if (clothName == "") {
+    alert("Add the data");
+  } else {
+    $("#clothes").append(
+      "<ul><li><a href='#'>" +
+        clothName +
+        "</li>"
+    );
   }
-   Trouser_l2 = () => {
-    category = prompt("Enter The Sub Category");
-    if (category != "") {
-      $("#l2").append(
-        "<ul><li><a href='#'>" +
-          category +
-          "</a><input type='button' value='+' onclick='Trouser_l2()'></li></ul>"
-      );
-    } else {
-      alert("Add atleast one item");
-    }
+}
+function addingElectronics() {
+  let electronicName = prompt("Electronic Name");
+  if (electronicName == "") {
+    alert("Add the data");
+  } else {
+    $("#electronics").append(
+      "<li><a href='#'>" +
+        electronicName +
+        "</a><button onclick='addingMoreElectronics()'>+</button></li>"
+    );
   }
-  Elec_Ul2 = () => {
-    category = prompt("Enter The Category");
-    if (category != "") {
-      $("#subul2").append(
-        "<li><a href='#'>" +
-          category +
-          "</a><input type='button' value='+' onclick='Elec_Ul2()'></li>"
-      );
-    } else {
-      alert("Add atleast one item");
-    }
+}
+function addingMoreElectronics(){
+   let electronicName = prompt("electronic Name");
+  if (electronicName == "") {
+    alert("Add the data");
+  } else {
+    $("#electronics").append(
+      "<ul><li><a href='#'>" +
+        electronicName +
+        "</li>"
+    );
   }
-  Mob_l3 = () => {
-    category = prompt("Enter The Sub Category");
-    if (category != "") {
-      $("#l3").append(
-        "<ul><li><a href='#'>" +
-          category +
-          "</a><input type='button' value='+' onclick='Mob_l3()'></li></ul>"
-      );
-    } else {
-      alert("Add atleast one item");
-    }
-  }
-   Tabs_l4 = () => {
-    category = prompt("Enter The Sub Category");
-    if (category != "") {
-      $("#l4").append(
-        "<ul><li><a href='#'>" +
-          category +
-          "</a><input type='button' value='+' onclick='Tabs_l4()'></li></ul>"
-      );
-    } else {
-      alert("Add atleast one item");
-    }
-  }
-});
+}
